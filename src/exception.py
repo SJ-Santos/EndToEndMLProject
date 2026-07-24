16dff3b81f5225e347ab3f582e037d58bf4ed60e
SJ-Santos <samuel.00000847429@unicap.br>
2026-07-24 16:23:20 -0300

    exception class

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,19 @@
 import sys
 
+#method for padronized error messages 
 def error_message_detail(error,error_detail:sys):
     exc_type,exc_value,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in Python script name [{0}] line number [{1}] error message [{2}] exception type [{3}] and exception value [{4}]".format(
         file_name,exc_tb.tb_lineno,str(error),exc_type,exc_value
     )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+    #like a getter method
+    def __str__(self):
+        return self.error_message
