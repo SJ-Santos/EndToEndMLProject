1f969914adf80fab3f7124ff555e26482e869213
SJ-Santos <samuel.00000847429@unicap.br>
2026-07-26 17:33:45 -0300

    logger and exception test

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 #method for padronized error messages 
 def error_message_detail(error,error_detail:sys):
@@ -17,3 +18,13 @@ class CustomException(Exception):
     #like a getter method
     def __str__(self):
         return self.error_message
+
+
+
+if __name__=="__main__":
+    try:
+        a = 1/0
+
+    except Exception as e:
+        logging.info("divided by zero")
+        raise CustomException(e,sys)
